d614b266f4690134d6873eeb8bb93af4ebe18c7b
Rakesh Pedapudi <rakeshpedapudi07@gmail.com>
2025-08-11 11:16:01 +0530

    Update __init__.py

---
+++ Jarvis-voice-assistant/src/__init__.py
@@ -0,0 +1,10 @@
+"""
+Jarvis Voice Assistant package.
+Import and run with:
+    from src import run_jarvis
+    run_jarvis()
+"""
+
+from .Jarvis import main as run_jarvis
+
+__all__ = ["run_jarvis"]
